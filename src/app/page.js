5e606ddd73aa5657c1f12ab4fd578cd097d0eae0
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-30 20:10:44 +0200

    add animation

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,11 @@
 "use client"
 import Image from "next/image";
 import { useState, useEffect } from 'react';
-import InititalLoading from "./components/InititalLoading";
+import dynamic from 'next/dynamic'
+
+// import InititalLoading from "./components/InititalLoading";
+const InititalLoading = dynamic(() => import('./components/InititalLoading'), { ssr: false })
+
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
 
@@ -9,7 +13,7 @@ export default function Home() {
     // Set a timeout to change isLoading to false after 3 seconds
     const timer = setTimeout(() => {
       setIsLoading(false);
-    }, 3000); // 3000ms = 3 seconds
+    }, 4000); // 3000ms = 3 seconds
 
     // Cleanup the timer when the component is unmounted
     return () => clearTimeout(timer);
@@ -19,6 +23,7 @@ export default function Home() {
   //   // Display a loading message or spinner while loading
   //   return <InititalLoading />;
   // }
+
   return (
     <div className={`min-h-screen flex flex-col gap-8 bg-[#0a192f] justify-center items-center relative `}>
       <InititalLoading />
