45f8a6a4bf08b1c81dc55c84bf4d0a0b30b1c5fe
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-31 12:42:09 +0200

    update index page

--- src/app/components/Search.js
+++ src/app/components/Search.js
@@ -2,22 +2,29 @@
 import { useRouter } from 'next/navigation';
 import React from 'react'
 import { useState, useEffect } from 'react';
-
+import Loading_v2 from './Loading_v2';
+import styles from "./styles/Search.module.scss"
 const Search = () => {
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        router.push(`/user?id=${username}`);
-    };
     const [username, setUsername] = useState('');
     const router = useRouter();
+    const [loading, setLoading] = useState(false)
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        // router.push(`/user?id=${username}`);
+        setLoading(true)
+    };
+
 
     return (
+        <>
+            <form onSubmit={handleSubmit}>
+                <div className={`${styles.container} `}>
+                    <input type="text" className={`${styles.input} ${loading ? styles.slideOut : null}`} placeholder="Search..." value={username} onChange={(e) => setUsername(e.target.value)} />
+                </div>
+            </form>
+            {loading ? <Loading_v2 /> : null}
 
-        <form onSubmit={handleSubmit}>
-            <div className={`search-container `}>
-                <input type="text" className="search-input" placeholder="Search..." value={username} onChange={(e) => setUsername(e.target.value)} />
-            </div>
-        </form>
+        </>
     )
 }
 
--- src/app/page.js
+++ src/app/page.js
@@ -4,15 +4,13 @@ import { useState, useEffect } from 'react';
 import dynamic from 'next/dynamic'
 import { useRouter } from 'next/navigation';
 import Search from "./components/Search";
+import Loading_v2 from "./components/Loading_v2";
 
 // import InititalLoading from "./components/InititalLoading";
 const InititalLoading = dynamic(() => import('./components/InititalLoading'), { ssr: false })
 
 export default function Home() {
   const [isLoading, setIsLoading] = useState(true);
-  const [username, setUsername] = useState('');
-
-  const router = useRouter();
 
   useEffect(() => {
     // Set a timeout to change isLoading to false after 3 seconds
@@ -24,14 +22,11 @@ export default function Home() {
     return () => clearTimeout(timer);
   }, []);
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    router.push(`/user?id=${username}`);
-  };
   return (
     <div className={`min-h-screen flex flex-col gap-8 bg-[#0a192f] justify-center items-center relative `}>
       <InititalLoading />
       <Search />
+
     </div>
   );
 }
