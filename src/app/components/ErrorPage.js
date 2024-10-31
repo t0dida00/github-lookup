91177be9d092edbbe66736e457573c9172c40255
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-10-31 17:29:04 +0200

    add back button

--- src/app/components/ErrorPage.js
+++ src/app/components/ErrorPage.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import styles from "./styles/ErrorPage.module.scss"
+import { useRouter } from 'next/navigation';
+
 const ErrorPage = () => {
+    const router = useRouter();
     return (
         <div className={styles.container}>
             <div className={styles.cont_principal}>
 
                 <h1>Oops !!!</h1>
                 <p>The profile you're looking for isn't here.</p>
+                <div className={styles.buttonBack}>
+                    <div onClick={() => router.push(`/`)}>
+                        Back to search
+                    </div>
 
+                </div>
                 {/* <div className="styles.cont_aura_1"></div>
             <div className={styles.cont_aura_2}></div> */}
             </div>
