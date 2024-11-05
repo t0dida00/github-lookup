a9bafe36dca696be983c97ff87c75180e19a64d2
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-11-05 15:37:10 +0200

    remove hover

--- src/app/user/[slug]/page.js
+++ src/app/user/[slug]/page.js
@@ -203,7 +203,7 @@ export default function Page({ params }) {
                             <div className='flex flex-wrap  gap-2'>
                                 {topics && topics.length > 0 && topics.map((tp, index) => <Topics topic={tp} key={index} selections={selections} setSelection={setSelection} />)}
                             </div>
-                            <div className='absolute w-2 h-2 top-3 right-4 group/svg transform hover:scale-150 cursor-pointer' onClick={() => handleClear('topics')}>
+                            <div className='absolute w-2 h-2 top-3 right-4 group/svg transform lg:hover:scale-150 cursor-pointer' onClick={() => handleClear('topics')}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill='white' className='lg:group-hover/svg:fill-[#64ffda]' d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" /></svg>
                             </div>
                         </div>
@@ -215,7 +215,7 @@ export default function Page({ params }) {
                             <div className='flex flex-wrap  gap-2'>
                                 {languages && languages.length > 0 && languages.map((lg, index) => <Languages language={lg} key={index} selections={selections} setSelection={setSelection} />)}
                             </div>
-                            <div className='absolute w-2 h-2 top-3 right-4 group/svg transform hover:scale-150 cursor-pointer' onClick={() => handleClear('languages')}>
+                            <div className='absolute w-2 h-2 top-3 right-4 group/svg transform lg:hover:scale-150 cursor-pointer' onClick={() => handleClear('languages')}>
                                 <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 384 512"><path fill='white' className='lg:group-hover/svg:fill-[#64ffda]' d="M376.6 84.5c11.3-13.6 9.5-33.8-4.1-45.1s-33.8-9.5-45.1 4.1L192 206 56.6 43.5C45.3 29.9 25.1 28.1 11.5 39.4S-3.9 70.9 7.4 84.5L150.3 256 7.4 427.5c-11.3 13.6-9.5 33.8 4.1 45.1s33.8 9.5 45.1-4.1L192 306 327.4 468.5c11.3 13.6 31.5 15.4 45.1 4.1s15.4-31.5 4.1-45.1L233.7 256 376.6 84.5z" /></svg>
                             </div>
                         </div>
