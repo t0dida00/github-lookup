c5b520aeb8621e9b4dcbe4966fdf6f21bcb1d830
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-11-05 10:44:45 +0200

    update hover

--- src/app/components/Languages.js
+++ src/app/components/Languages.js
@@ -14,7 +14,7 @@ const Languages = (props) => {
     return (
 
         <div className='flex items-center rounded-full bg-[#ddf4ff] px-3 py-1 text-xs 
-    font-semibold leading-5 text-[#0969da] hover:bg-[#0969da] hover:text-white transition duration-200 cursor-pointer h-fit' onClick={handleSelect}
+    font-semibold leading-5 text-[#0969da] lg:hover:bg-[#0969da] lg:hover:text-white transition duration-200 cursor-pointer h-fit' onClick={handleSelect}
             style={selections.includes(language) ? { backgroundColor: languageColor, color: 'white' } : {}}
         >{
                 language
--- src/app/components/Topics.js
+++ src/app/components/Topics.js
@@ -12,7 +12,7 @@ const Topics = (props) => {
     return (
 
         <div className='flex items-center rounded-full bg-[#ddf4ff] px-3 py-1 text-xs 
-                            font-semibold leading-5 text-[#0969da] hover:bg-[#0969da] hover:text-white transition duration-200 cursor-pointer h-fit' onClick={handleSelect}
+                            font-semibold leading-5 text-[#0969da] lg:hover:bg-[#0969da] lg:hover:text-white transition duration-200 cursor-pointer h-fit' onClick={handleSelect}
             style={selections.includes(topic) ? { backgroundColor: '#0969da', color: 'white' } : {}}>{topic}</div>
 
     )
