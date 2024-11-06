c1c3521d6103e8ec347e7f00d429ca1a1bc46450
t0dida00 <70305254+t0dida00@users.noreply.github.com>
2024-11-06 11:42:04 +0200

    fix error

--- src/app/components/ErrorPage.js
+++ src/app/components/ErrorPage.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import styles from '/styles/ErrorPage.module.scss'
+import styles from './styles/ErrorPage.module.scss'
 import { useRouter } from 'next/navigation';
 
 const ErrorPage = ({ error }) => {
--- src/app/not-found.js
+++ src/app/not-found.js
@@ -1,6 +1,6 @@
 'use client'
 import React from 'react'
-import styles from "./ErrorPage.module.scss"
+import styles from "./components/styles/ErrorPage.module.scss"
 import { useRouter } from 'next/navigation';
 
 const Custom404 = ({ error }) => {
